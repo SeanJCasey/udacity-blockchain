1f566487f5719b3b09a7c2619af3acefeb3d863f
Sean Casey <sean@seanjcasey.com>
2019-10-19 18:59:07 +0900

    update test config with contract args

--- 7_flight_insurance/config/testConfig.js
+++ 7_flight_insurance/config/testConfig.js
@@ -1,10 +1,8 @@
-
 var FlightSuretyApp = artifacts.require("FlightSuretyApp");
 var FlightSuretyData = artifacts.require("FlightSuretyData");
 var BigNumber = require('bignumber.js');
 
 var Config = async function(accounts) {
-    
     // These test addresses are useful when you need to add
     // multiple users in test scripts
     let testAddresses = [
@@ -23,10 +21,9 @@ var Config = async function(accounts) {
     let owner = accounts[0];
     let firstAirline = accounts[1];
 
-    let flightSuretyData = await FlightSuretyData.new();
-    let flightSuretyApp = await FlightSuretyApp.new();
+    let flightSuretyData = await FlightSuretyData.new(firstAirline);
+    let flightSuretyApp = await FlightSuretyApp.new(flightSuretyData.address);
 
-    
     return {
         owner: owner,
         firstAirline: firstAirline,

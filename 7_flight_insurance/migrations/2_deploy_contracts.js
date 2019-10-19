c63aa804cfaa8edd4305957b5ea6460c59fd5903
Sean Casey <sean@seanjcasey.com>
2019-10-19 18:56:46 +0900

    update FlightSurety migrations with the proper constructor args, initial state values, and mock flights

--- 7_flight_insurance/migrations/2_deploy_contracts.js
+++ 7_flight_insurance/migrations/2_deploy_contracts.js
@@ -2,22 +2,36 @@ const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
 const fs = require('fs');
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
+    const firstAirline = accounts[1];
+    let appInstance, dataInstance;
 
-    let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-    deployer.deploy(FlightSuretyData)
-    .then(() => {
-        return deployer.deploy(FlightSuretyApp)
-                .then(() => {
-                    let config = {
-                        localhost: {
-                            url: 'http://localhost:8545',
-                            dataAddress: FlightSuretyData.address,
-                            appAddress: FlightSuretyApp.address
-                        }
-                    }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                });
-    });
+    // Deploy data contract with first airline
+    deployer.deploy(FlightSuretyData, firstAirline)
+        .then(instance => dataInstance = instance)
+        // Deploy app contract with data contract address
+        .then(() => deployer.deploy(FlightSuretyApp, FlightSuretyData.address))
+        .then(instance => appInstance = instance)
+        // Initialize app address in data contract
+        .then(() => dataInstance.authorizeCaller(FlightSuretyApp.address))
+        // Ante up for firstAirline
+        .then(() => dataInstance.fund({ from: firstAirline, value: web3.utils.toWei("10", "ether") }))
+        // Add 3 mock flights
+        .then(() => {
+            appInstance.registerFlight("U150", 1571220000, { from: firstAirline });
+            appInstance.registerFlight("U150", 1571306400, { from: firstAirline });
+            appInstance.registerFlight("U150", 1571392800, { from: firstAirline });
+        })
+        // Export config file with contract addresses
+        .then(() => {
+            const config = {
+                localhost: {
+                    url: 'http://localhost:8545',
+                    dataAddress: FlightSuretyData.address,
+                    appAddress: FlightSuretyApp.address
+                }
+            }
+            fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+            fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+        })
 }

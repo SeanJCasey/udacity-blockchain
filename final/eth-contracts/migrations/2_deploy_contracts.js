738ca20f26b28bb6bc16afddb90bec2f663bdd78
Sean Casey <sean@seanjcasey.com>
2019-10-21 20:31:40 +0900

    update migrations

--- final/eth-contracts/migrations/2_deploy_contracts.js
+++ final/eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 // migrating the appropriate contracts
-var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+const PropertyToken = artifacts.require("./PropertyToken.sol");
+// var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+// var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+  deployer.deploy(PropertyToken);
+  // deployer.deploy(SquareVerifier);
+  // deployer.deploy(SolnSquareVerifier);
 };

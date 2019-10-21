c1e9b3aa7fe51e811c00030e9804b3d6d079a957
Sean Casey <sean@seanjcasey.com>
2019-10-21 20:32:01 +0900

    add tests for PropertyToken

---
+++ final/eth-contracts/test/TestPropertyToken.js
@@ -0,0 +1,63 @@
+const PropertyToken = artifacts.require('PropertyToken');
+
+contract('TestPropertyToken', accounts => {
+
+    const account_one = accounts[0];
+    const account_two = accounts[1];
+
+    describe('match erc721 spec', function () {
+        beforeEach(async function () {
+            this.contract = await PropertyToken.new({from: account_one});
+
+            // TODO: mint multiple tokens
+            this.contract.mint(account_one, 1, "test1", { from: account_one });
+            this.contract.mint(account_two, 2, "test2", { from: account_one });
+        })
+
+        it('should return total supply', async function () {
+            const totalSupply = await this.contract.totalSupply();
+            assert.equal(totalSupply, 2, `Total supply should be 2, got ${totalSupply}`);
+        })
+
+        it('should get token balance', async function () {
+            const tokenBalance = await this.contract.balanceOf(account_two);
+            assert.equal(tokenBalance, 1, `Token balance should be 1, got ${tokenBalance}`);
+        })
+
+        // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
+        it('should return token uri', async function () {
+            const tokenURI = await this.contract.tokenURI(1);
+            const expectedURI = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/test1";
+            assert.equal(tokenURI, expectedURI, `Unexpected token URI: ${tokenURI}`);
+        })
+
+        it('should transfer token from one owner to another', async function () {
+            await this.contract.transferFrom(account_one, account_two, 1);
+            const tokenBalance = await this.contract.balanceOf(account_two);
+            assert.equal(tokenBalance, 2, `Token balance should be 2, got ${tokenBalance}`);
+        })
+    });
+
+    describe('have ownership properties', function () {
+        beforeEach(async function () {
+            this.contract = await PropertyToken.new({from: account_one});
+        })
+
+        it('should fail when minting when address is not contract owner', async function () {
+            let didThrow = false;
+            try {
+                await this.contract.mint(account_two, 1, "test1", { from: account_two });
+            }
+            catch(e) {
+                didThrow = true;
+            }
+            assert.equal(didThrow, true, "Should have thrown error minting as user 2")
+        })
+
+        it('should return contract owner', async function () {
+            const owner = await this.contract.getOwner();
+            assert.equal(owner, account_one, "Account one should be owner");
+        })
+
+    });
+})

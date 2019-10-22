32e675c17dc694de3b0653d355c6d68f3229059a
Sean Casey <sean@seanjcasey.com>
2019-10-22 09:27:56 +0900

    add placeholder for SolnSquareVerifier contract

--- final/eth-contracts/migrations/2_deploy_contracts.js
+++ final/eth-contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 // migrating the appropriate contracts
 const PropertyToken = artifacts.require("./PropertyToken.sol");
-// var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-// var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+const SquareVerifier = artifacts.require("./SquareVerifier.sol");
+const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(PropertyToken);
-  // deployer.deploy(SquareVerifier);
-  // deployer.deploy(SolnSquareVerifier);
+  deployer.deploy(SquareVerifier);
+  deployer.deploy(SolnSquareVerifier);
 };
